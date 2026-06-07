193ba38fa8f6013903d13e37433f969e00a862e6
gayan <gayan@FLTP-5R7-16512>
2026-06-07 18:12:07 +0300

    Создала папку Models, классы Film.cs и Studio.cs

---
+++ IDZ3/Models/Studio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using static System.Net.WebRequestMethods;
+
+namespace IDZ3.Models
+{
+    /// <summary>
+    /// Класс, представляющий киностудию.
+    /// Каждый объект Studio = одна запись в таблице Studios.
+    /// Это СПРАВОЧНИК (master table).
+    /// </summary>
+    public class Studio
+    {
+        /// <summary>
+        /// Уникальный идентификатор студии.
+        /// Это первичный ключ (Primary Key) в базе данных.
+        /// EF Core сам поймёт, что Id - это ключ.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название киностудии. Например, "Warner Bros", "Universal".
+        /// Это поле обязательно для заполнения (NOT NULL в БД).
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Навигационное свойство - список фильмов, которые сняла эта студия.
+        /// EF Core использует это для связи "один ко многим".
+        /// </summary>
+        public List<Film> Films { get; set; } = new List<Film>();
+    }
+}
